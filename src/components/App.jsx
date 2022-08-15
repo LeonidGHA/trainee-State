4d22fabcfe898e9f7df85b415f21ce7a7f9fb3d5
LeonidGHA <stowk1992@gmail.com>
2022-08-15 14:09:40 +0300

    push new rep

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,3 @@
-import Profile from './Profile/Profile';
-import Statistics from './Statistics/Statistics';
-import FriendList from './FriendList/FriendList';
-import TransactionHistory from './TransactionHistory/TransactionHistory';
-
-import css from './App.module.css';
-
-import user from '../Data/user.json';
-import data from '../Data/data.json';
-import friends from '../Data/friends.json';
-import transactions from '../Data/transactions.json';
-
 export const App = () => {
   return (
     <div className={css.rootStylization}>
